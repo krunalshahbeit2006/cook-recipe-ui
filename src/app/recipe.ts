22440735864c6b1874cf0f81496a526e7a2a81e9
ShahKA <krunal.shah@rabobank.nl>
2018-07-19 14:49:30 +0200

    Cooking Recipe UI - Required dependencies are injected in constructor

--- src/app/recipe.ts
+++ src/app/recipe.ts
@@ -4,4 +4,10 @@ export class Recipe {
   id: number;
   name: string;
   recipeDetail: RecipeDetail;
+
+  constructor(id: number, name: string, recipeDetail: RecipeDetail) {
+    this.id = id;
+    this.name = name;
+    this.recipeDetail = recipeDetail;
+  }
 }
--- src/app/recipeDetail.ts
+++ src/app/recipeDetail.ts
@@ -1,6 +1,6 @@
 export enum IndicatorType {
   Veg = "Vegetarian",
-  NonVeg = "Non Vegetarian",
+  NonVeg = "Non-vegetarian",
 }
 
 export class RecipeDetail {
@@ -10,4 +10,14 @@ export class RecipeDetail {
   numberOfPeopleDishSuitableFor: number;
   listOfIngredients: Array<String>;
   cookingInstructions: Array<String>;
+
+  constructor(dateOfCreation: Date, imageSrcUrl: string, typeOfDish: IndicatorType,
+              numberOfPeopleDishSuitableFor: number, listOfIngredients: Array<String>, cookingInstructions: Array<String>) {
+    this.dateOfCreation = dateOfCreation;
+    this.imageSrcUrl = imageSrcUrl;
+    this.typeOfDish = typeOfDish;
+    this.numberOfPeopleDishSuitableFor = numberOfPeopleDishSuitableFor;
+    this.listOfIngredients = listOfIngredients;
+    this.cookingInstructions = cookingInstructions;
+  }
 }
