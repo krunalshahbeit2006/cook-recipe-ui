187807389a7d0d612fcb33a8a81e1363abc21928
ShahKA <krunal.shah@rabobank.nl>
2018-07-20 11:23:25 +0200

    Cooking Recipe UI - Test/s updated

--- src/app/recipe-detail/recipe-detail.component.spec.ts
+++ src/app/recipe-detail/recipe-detail.component.spec.ts
@@ -1,8 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {Recipe} from "../recipe";
-import {RecipeDetail, IndicatorType} from "../recipeDetail";
 import { RecipeDetailComponent } from './recipe-detail.component';
+
+import { RECIPES } from '../mock-recipes';
 
 describe('RecipeDetailComponent', () => {
   let component: RecipeDetailComponent;
@@ -18,15 +18,9 @@ describe('RecipeDetailComponent', () => {
   }));
 
   beforeEach(() => {
-    let recipe: Recipe;
-    let recipeDetail: RecipeDetail;
-
     fixture = TestBed.createComponent(RecipeDetailComponent);
     component = fixture.componentInstance;
-
-    recipeDetail = new RecipeDetail(new Date("01/07/2018"), "../assets/images/recipes/11.jpg", IndicatorType.Veg, 2, ["1/4 cup butter shopping list", "3/4 cup milk", "3/4 cup flour", "3 eggs", "1 cup apple filling (or another flavor)"], ["Melt butter in an 8x8 inch pan", "Mix milk, flour, and eggs until smooth", "Pour into pan", "Spoon on apple filling", "Bake at 400 degrees Fahrenheit for 30 minutes", "Serve immediately, it will fall"]);
-    recipe = new Recipe(11, "Pannenkoeken", recipeDetail);
-    component.recipe = recipe;
+    component.recipe = RECIPES[0];
     fixture.detectChanges();
   });
 
